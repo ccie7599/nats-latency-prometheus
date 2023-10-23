11babe8514b874eadc1e56bc4b0e4592f286536e
ccie7599 <brian@apley.net>
2023-10-22 21:55:05 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -131,14 +131,14 @@ credentialSecret: false,
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$/yJYdVj2P9jEQknzWX13/eEv5Gk7CD6uWS377Qm6njIh7PUq3zRZC",
+            permissions: "*"
+        }]
+    },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
